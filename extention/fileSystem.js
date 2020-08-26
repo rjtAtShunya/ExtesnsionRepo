d2a36ad7cb32dcb0805524a80cbecd071ebeab61
rjtAtShunya <69190984+rjtAtShunya@users.noreply.github.com>
2020-08-26 04:51:39 -0700

    Add files via upload

--- extention/fileSystem.js
+++ extention/fileSystem.js
@@ -1,3 +1,29 @@
+//function to upload file to google drive
+function uploadtoDrive(fileContent){
+var fileContent = fileContent; // As a sample, upload a text file.
+var file = new Blob([fileContent], {type: 'text/plain'});
+var metadata = {
+    'name': 'sampleName', // Filename at Google Drive
+    'mimeType': 'text/plain', // mimeType at Google Drive
+    'parents': ['1faJ9NdNqOSxR5d0bgWiNEUkVoXChRVmf'], // Folder ID at Google Drive
+};
+
+var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+var form = new FormData();
+form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+form.append('file', file);
+
+var xhr = new XMLHttpRequest();
+xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+xhr.responseType = 'json';
+xhr.onload = () => {
+    console.log(xhr.response.id); // Retrieve uploaded file ID.
+};
+xhr.send(form);
+}
+
+
 
 // Allow for vendor prefixes.
 window.requestFileSystem = window.requestFileSystem ||
@@ -131,6 +157,7 @@ function listFiles() {
   var fetchEntries = function() {
     dirReader.readEntries(function(results) {
       if (!results.length) {
+        
         displayEntries(entries.sort().reverse());
       } else {
         entries = entries.concat(results);
@@ -205,6 +232,7 @@ var name = "config.json"
     // console.log(data)
     var data = JSON.parse(localStorage.getItem('data'));
      data = JSON.stringify(data)
+     uploadtoDrive(data);
     saveFile(name, data);
     console.log("file saved with data:",data)
     // Save the file.
