2eacac2325a5c008d07bf9c4e48d531f51b58fc0
rjtAtShunya <69190984+rjtAtShunya@users.noreply.github.com>
2020-08-27 22:22:44 -0700

    Add files via upload

--- extention/fileSystem.js
+++ extention/fileSystem.js
@@ -1,33 +1,70 @@
-//function to upload file to google drive
-function uploadtoDrive(fileContent){
-var fileContent = fileContent; // As a sample, upload a text file.
-var file = new Blob([fileContent], {type: 'text/plain'});
-var metadata = {
-    'name': 'sampleName', // Filename at Google Drive
-    'mimeType': 'text/plain', // mimeType at Google Drive
-    'parents': ['1faJ9NdNqOSxR5d0bgWiNEUkVoXChRVmf'], // Folder ID at Google Drive
-};
 
-var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
-var form = new FormData();
-form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
-form.append('file', file);
 
-var xhr = new XMLHttpRequest();
-xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
-xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-xhr.responseType = 'json';
-xhr.onload = () => {
-    console.log(xhr.response.id); // Retrieve uploaded file ID.
-};
-xhr.send(form);
+
+
+
+const API_KEY = 'AIzaSyBHkW6_NwdFgMSr7NBqEiljCICosyyLM-o';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+
+// const SPREADSHEET_ID = 'YOUR_SPREADSHEET_ID';
+// const SPREADSHEET_TAB_NAME = 'main';
+
+function onGAPILoad() {
+  var data = JSON.parse(localStorage.getItem('data'));
+  data = JSON.stringify(data)
+  gapi.client.init({
+    // Don't pass client nor scope as these will init auth2, which we don't want
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  }).then(function () {
+    console.log('gapi initialized')
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      gapi.auth.setToken({
+        'access_token': token,
+      });
+
+
+      var fileContent = 'sample text'; // As a sample, upload a text file.
+      var file = new Blob([fileContent], { type: 'text/plain' });
+      var metadata = {
+        'name': 'sampleName', // Filename at Google Drive
+        'mimeType': 'text/plain', // mimeType at Google Drive
+        'parents': ['https://drive.google.com/drive/folders/1Oyxp3mQ5TSWfq_Wn83EmMEHLwi7wAkhb'], // Folder ID at Google Drive
+      };
+
+      var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+      var form = new FormData();
+      form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+      form.append('file', file);
+
+      fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
+        method: 'POST',
+        headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
+        body: form,
+      }).then((res) => {
+        return res.json();
+      }).then(function (val) {
+        console.log(val);
+      });
+    });
+  })
+    , function (error) {
+      console.log('error', error)
+    };
 }
+
+
+
+
+
+
 
 
 
 // Allow for vendor prefixes.
 window.requestFileSystem = window.requestFileSystem ||
-                           window.webkitRequestFileSystem;
+  window.webkitRequestFileSystem;
 
 
 // Create a variable that will store a reference to the FileSystem.
@@ -46,7 +83,7 @@ var messageBox = document.getElementById('messages');
 // A simple error handler to be used throughout this demo.
 function errorHandler(error) {
   var message = '';
-
+  FileError = error
   switch (error.code) {
     case FileError.SECURITY_ERR:
       message = 'Security Error';
@@ -75,14 +112,14 @@ function errorHandler(error) {
 // Request a FileSystem and set the filesystem variable.
 function initFileSystem() {
   navigator.webkitPersistentStorage.requestQuota(1024 * 1024 * 5,
-    function(grantedSize) {
+    function (grantedSize) {
 
       // Request a file system with the new size.
-      window.requestFileSystem(window.PERSISTENT, grantedSize, function(fs) {
+      window.requestFileSystem(window.PERSISTENT, grantedSize, function (fs) {
 
         // Set the filesystem variable.
         filesystem = fs;
-
+        // function onGAPILoad()
         // Setup event listeners on the form.
         setupFormEventListener();
 
@@ -96,12 +133,12 @@ function initFileSystem() {
 
 
 function loadFile(filename) {
-  filesystem.root.getFile(filename, {}, function(fileEntry) {
+  filesystem.root.getFile(filename, {}, function (fileEntry) {
 
-    fileEntry.file(function(file) {
+    fileEntry.file(function (file) {
       var reader = new FileReader();
 
-      reader.onload = function(e) {
+      reader.onload = function (e) {
         // Update the form fields.
         filenameInput.value = filename;
         contentTextArea.value = this.result;
@@ -118,7 +155,7 @@ function displayEntries(entries) {
   // Clear out the current file browser entries.
   fileList.innerHTML = '';
 
-  entries.forEach(function(entry, i) {
+  entries.forEach(function (entry, i) {
     var li = document.createElement('li');
 
     var link = document.createElement('a');
@@ -135,14 +172,14 @@ function displayEntries(entries) {
 
     // Setup an event listener that will load the file when the link
     // is clicked.
-    link.addEventListener('click', function(e) {
+    link.addEventListener('click', function (e) {
       e.preventDefault();
       loadFile(entry.name);
     });
 
     // Setup an event listener that will delete the file when the delete link
-    // is clicked.
-    delLink.addEventListener('click', function(e) {
+    // is clicke/d.
+    delLink.addEventListener('click', function (e) {
       e.preventDefault();
       deleteFile(entry.name);
     });
@@ -154,10 +191,10 @@ function listFiles() {
   var dirReader = filesystem.root.createReader();
   var entries = [];
 
-  var fetchEntries = function() {
-    dirReader.readEntries(function(results) {
+  var fetchEntries = function () {
+    dirReader.readEntries(function (results) {
       if (!results.length) {
-        
+
         displayEntries(entries.sort().reverse());
       } else {
         entries = entries.concat(results);
@@ -173,11 +210,11 @@ function listFiles() {
 
 // Save a file in the FileSystem.
 function saveFile(filename, content) {
-  filesystem.root.getFile(filename, {create: true}, function(fileEntry) {
+  filesystem.root.getFile(filename, { create: true }, function (fileEntry) {
 
-    fileEntry.createWriter(function(fileWriter) {
+    fileEntry.createWriter(function (fileWriter) {
 
-      fileWriter.onwriteend = function(e) {
+      fileWriter.onwriteend = function (e) {
         // Update the file browser.
         listFiles();
 
@@ -189,12 +226,12 @@ function saveFile(filename, content) {
         messageBox.innerHTML = 'File saved!';
       };
 
-      fileWriter.onerror = function(e) {
+      fileWriter.onerror = function (e) {
         console.log('Write error: ' + e.toString());
         alert('An error occurred and your file could not be saved!');
       };
 
-      var contentBlob = new Blob([content], {type: 'json'});
+      var contentBlob = new Blob([content], { type: 'json' });
 
       fileWriter.write(contentBlob);
 
@@ -205,9 +242,9 @@ function saveFile(filename, content) {
 
 
 function deleteFile(filename) {
-  filesystem.root.getFile(filename, {create: false}, function(fileEntry) {
+  filesystem.root.getFile(filename, { create: false }, function (fileEntry) {
 
-    fileEntry.remove(function(e) {
+    fileEntry.remove(function (e) {
       // Update the file browser.
       listFiles();
 
@@ -221,20 +258,20 @@ function deleteFile(filename) {
 
 // Add event listeners on the form.
 function setupFormEventListener() {
-   
-  form.addEventListener('submit', function(e) {
+
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     // Get the form data.
     var filename = filenameInput.value;
     var content = contentTextArea.value;
-var name = "config.json"
+    var name = "config.json"
     // console.log(data)
     var data = JSON.parse(localStorage.getItem('data'));
-     data = JSON.stringify(data)
-     uploadtoDrive(data);
+    data = JSON.stringify(data)
+    // upload toDrive(data);
     saveFile(name, data);
-    console.log("file saved with data:",data)
+    console.log("file saved with data:", data)
     // Save the file.
     saveFile(filename, content);
   });
